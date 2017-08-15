ab48c7375030e5f8f1bc7a9d4f91ba281ed7cae6
Giselelasserre <giseleslasserre@gmail.com>
2017-08-15 17:18:21 -0300

    schema gemgile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,8 @@
 
 ActiveRecord::Schema.define(version: 20170815182800) do
 
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "flats", force: :cascade do |t|
     t.string   "location"
@@ -28,7 +25,6 @@ ActiveRecord::Schema.define(version: 20170815182800) do
     t.string   "title"
   end
 
-
   create_table "reservations", force: :cascade do |t|
     t.string   "dates"
     t.string   "references"
@@ -37,7 +33,6 @@ ActiveRecord::Schema.define(version: 20170815182800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
