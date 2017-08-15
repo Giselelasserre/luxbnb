6ac8fa47f2cbbcda41d51453fe71c2d3e7b6f299
Armando Ramirez <armando.p.ramirez@gmail.com>
2017-08-15 16:30:33 -0300

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170815173234) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "flats", force: :cascade do |t|
     t.string   "location"
@@ -27,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170815173234) do
     t.datetime "updated_at",  null: false
     t.string   "title"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
