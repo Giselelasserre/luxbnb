d15d63729e1cd1f78639a9fe16747224b723d17a
Giselelasserre <giseleslasserre@gmail.com>
2017-08-18 17:42:34 -0300

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,31 @@
 user = User.first
 
 Flat.create(
-  location: "Sao Paulo"
+  location: "Sao Paulo",
   description:"Nice flat",
   price: 10_000,
-  title: "Super Flat in Bangkok.",
+  title: "Super Flat in Sao Paulo.",
   picture: "http://www.phadke-developers.com/images/projects/tathastu/large/2-bhk-bedroom-apartment-flat-for-sale-in-sangli.jpg",
   address: "Av. 9 de julho, 2000, São Paulo, SP",
+  user_id: user.id
+)
+
+Flat.create(
+  location: "Sao Paulo",
+  description:"Flat for great people",
+  price: 10_000,
+  title: "Super Flat in SP.",
+  picture: "http://www.phadke-developers.com/images/projects/tathastu/large/2-bhk-bedroom-apartment-flat-for-sale-in-sangli.jpg",
+  address: "Rua Canario, 981, São Paulo, SP",
+  user_id: user.id
+)
+
+Flat.create(
+  location: "Sao Paulo",
+  description:"Elegant flat",
+  price: 10_000,
+  title: "Super Flat in SP.",
+  picture: "http://www.phadke-developers.com/images/projects/tathastu/large/2-bhk-bedroom-apartment-flat-for-sale-in-sangli.jpg",
+  address: "Av. Paulista, 2000, São Paulo, SP",
   user_id: user.id
 )
