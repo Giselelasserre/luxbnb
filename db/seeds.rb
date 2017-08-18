1421e35fa9acfa13cd674bdcba8b68c89c2e3631
Giselelasserre <giseleslasserre@gmail.com>
2017-08-18 17:39:08 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+user = User.first
+
 Flat.create(
   location: "Sao Paulo"
   description:"Nice flat",
   price: 10_000,
   title: "Super Flat in Bangkok.",
   picture: "http://www.phadke-developers.com/images/projects/tathastu/large/2-bhk-bedroom-apartment-flat-for-sale-in-sangli.jpg",
-  address: "Av. 9 de julho, 2000, São Paulo, SP"
+  address: "Av. 9 de julho, 2000, São Paulo, SP",
+  user_id: user.id
 )
